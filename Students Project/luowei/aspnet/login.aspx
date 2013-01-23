24655db31196f8f8ced72a8b6390db342781a3cb
罗蔚 <550804648@QQ.COM>
2013-01-23 19:30:18 +0800

    forms验证原理

---
+++ Students Project/luowei/aspnet/login.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="login.aspx.cs" Inherits="login" %>
+
+<!DOCTYPE html>
+<script runat="server">
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+       
+    }
+</script>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="是否记住cookie" />
+    </form>
+</body>
+</html>
---
+++ Students Project/luowei/aspnet/login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        string userName = TextBox1.Text.Trim();
+        string userPwd = TextBox2.Text.Trim();
+        SqlConnection con = new SqlConnection(@"Data Source=LW\LW;Initial Catalog=MyForms;Integrated Security=True;Pooling=False");
+        SqlCommand cmd = new SqlCommand("select count(*) from users where userName='" + userName + "' and userPwd='" + userPwd + "'", con);
+        con.Open();
+
+        int count = (int)cmd.ExecuteScalar();
+        if (count > 0)
+        {
+            System.Web.Security.FormsAuthentication.SetAuthCookie(this.TextBox1.Text, this.CheckBox1.Checked);
+            Response.Redirect("Default.aspx");
+
+            con.Close();
+        }
+        else
+        {
+            Response.Write("用户不合法");
+        }
+    }
+}
